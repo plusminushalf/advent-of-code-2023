17ef4cf7a672ef7f684cf19365cf64c212e1c365
plusminushalf <garvitdelhi@gmail.com>
2023-12-07 09:08:10 +0530

    make reading file a function

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -1,19 +1,23 @@
 use std::{collections::HashMap, fs};
 
-fn iterate_file(filename: &str) -> String {
-    return fs::read_to_string(filename).expect("Something went wrong reading the file");
-}
-
-pub fn part1(filename: &str) -> u32 {
-    iterate_file(filename)
+fn iterate_file(filename: &str) -> Vec<Vec<String>> {
+    return fs::read_to_string(filename)
+        .expect("Something went wrong reading the file")
         .lines()
         .map(|line| {
             line.split(":")
                 .nth(1)
                 .unwrap()
                 .split("|")
-                .collect::<Vec<&str>>()
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>()
         })
+        .collect::<Vec<Vec<String>>>();
+}
+
+pub fn part1(filename: &str) -> u32 {
+    iterate_file(filename)
+        .iter()
         .map(|line| {
             let winning_numbers: HashMap<u32, u32> = line[0]
                 .split_whitespace()
@@ -40,14 +44,7 @@ pub fn part2(filename: &str) -> u32 {
     let mut game_count_map: HashMap<usize, u32> = HashMap::new();
 
     iterate_file(filename)
-        .lines()
-        .map(|line| {
-            line.split(":")
-                .nth(1)
-                .unwrap()
-                .split("|")
-                .collect::<Vec<&str>>()
-        })
+        .iter()
         .enumerate()
         .for_each(|(index, game)| {
             let winning_numbers: HashMap<u32, u32> = game[0]
